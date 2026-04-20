9430d057078fac048e266a6975d68e743018ffca
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2026-04-20 06:02:57 -0300

    Fix Docker runtime Spacetime startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,6 +19,6 @@ deploy() {
 trap "kill 0" SIGINT
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile \
-& spacetimedb start --listen-addr 0.0.0.0:3000 /stdb \
+& spacetime start --listen-addr 0.0.0.0:3000 --data-dir /stdb \
 & deploy \
 && wait
